31cb45c00268d5f87e0230810d9ac9fcd5d06a01
Ashwathaman <ashwathaman.r6@gmail.com>
2025-03-20 13:32:08 +0530

    image path change

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -13,14 +13,14 @@ function NavigationBar(props) {
 
     const cardImages = {
         "className": "addTocardimg px-1",
-        "src": "src/assets/card.svg",
+        "src": "./src/assets/card.svg",
         "alt": "addToCard"
     }
 
     return (
         <>
             <nav className="navBar flex justify-between items-center">
-                <img src="src\assets\eProd logo.png" alt="eProd" className="eProdLogo" />
+                <img src="./src/assets/eProd logo.png" alt="eProd" className="eProdLogo" />
                 <Buttons onClick={props.onClick} className={pageButtons.ClassName} insideText={<><Images src={cardImages.src} alt={cardImages.alt} className={cardImages.className}
                 />{pageButtons.insideText}</>} />
                 <div className="selectedProCount bg-red-600"><p className="text-white poppins-bold text-xs">{props.selectedProdectCount}</p></div>
